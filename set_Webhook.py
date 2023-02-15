c59615f0f7c3a7142bd82855bf67e75d09946d08
Doniyorbek <ndoniyorbek1@gmail.com>
2023-02-15 10:12:19 +0500

    set Webhook

---
+++ set_Webhook.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+url = 'https://ndu.pythonanywhere.com/webhook'
+
+TOKEN = os.environ["TOKEN"]
+
+payload = {
+    "url":url
+}
+
+r = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook", params=payload)
+
+
+print(r.status_code)
